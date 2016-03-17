0141b78cdd01454f9432c928dd678784f9136b59
Darrin Meek <darrinmeek@aol.com>
2016-03-17 08:53:00 -0400

    March 2016 updates

---
+++ synchme.sh
@@ -0,0 +1,30 @@
+
+TAGA_DIR=~/scripts/taga
+TAGA_CONFIG_DIR=$TAGA_DIR/tagaConfig
+source $TAGA_CONFIG_DIR/config
+
+MYDIR=`pwd`
+
+echo 
+echo $targetList
+
+for target in $targetList
+do
+
+   if [ $MYIP == $target ]; then
+      echo skipping self...
+   else
+      echo processing, synchronizing $target
+      sleep 1
+#      ssh -l $MYLOGIN_ID $target date
+#      ssh -l $MYLOGIN_ID $target hostname
+      # build the source file string
+      SCP_SOURCE_STR="."
+      # send the files to the destination
+      ssh -l $MYLOGIN_ID $target mkdir -p $MYDIR
+      scp -r $SCP_SOURCE_STR $MYLOGIN_ID@$target:$MYDIR \
+              <$TAGA_CONFIG_DIR/passwd.txt
+   fi
+
+done
+
