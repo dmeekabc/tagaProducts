a62f44261cce4a9c6adcc3e20b7986fbdf6c2853
Darrin Meek <darrinmeek@aolcom>
2016-03-17 12:25:54 -0400

    March 2016 updates

--- tagaScripts/tagaScriptsRun/runLoop.sh
+++ tagaScripts/tagaScriptsRun/runLoop.sh
@@ -465,7 +465,7 @@ do
    $tagaScriptsStatsDir/countSends.sh $outputDir $iter $startTime $currentDelta $deltaEpoch $startDTG
    $tagaScriptsStatsDir/countReceives.sh $outputDir $iter $startTime $startDTG 
 
-   for i in 1 2 3 4 5 6 # 7 8 9 10 11
+   for i in 1 2 #3 4 5 6 # 7 8 9 10 11
    do
       let ticker=6-$i
       echo Configuration Change Window: Change Configuration Now... $ticker
