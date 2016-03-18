b77ae20617f9b92574bec723fee144f912260dfe
Darrin Meek <darrinmeek@aolcom>
2016-03-18 08:19:59 -0400

    March 2016 updates

--- tagaScripts/tagaScriptsUtils/confirmSample.sh
+++ tagaScripts/tagaScriptsUtils/confirmSample.sh
@@ -7,8 +7,6 @@ TAGA_DIR=~/scripts/taga
 TAGA_CONFIG_DIR=$TAGA_DIR/tagaConfig
 source $TAGA_CONFIG_DIR/config
 
-echo; echo $0 : $MYIP :  executing at `date`; echo
-
 # issue confirmation prompt and check reponse
 $tagaScriptsUtilsDir/confirmWrapper.sh $0 "Info to Print"
 # check the response
