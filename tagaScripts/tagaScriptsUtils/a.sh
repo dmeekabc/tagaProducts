cacafb5ce40a6669ee91d807d276c1d40cf54c54
Darrin Meek <darrinmeek@aolcom>
2016-03-18 12:37:03 -0400

    March 2016 updates

--- tagaScripts/tagaScriptsUtils/a.sh
+++ tagaScripts/tagaScriptsUtils/a.sh
@@ -9,7 +9,14 @@ source $TAGA_CONFIG_DIR/config
 
 echo; echo $0 : $MYIP :  executing at `date`; echo
 
+# provide the info to print into the confirmation request
+InfoToPrint="$0 Put Your Info To Print Here. $0 "
+
 # issue confirmation prompt and check reponse
-$tagaUtilsDir/confirm.sh $0 "Info to Print"
-# check the response
+$tagaUtilsDir/confirm.sh $0 "$InfoToPrint"
 response=$?; if [ $response -ne 1 ]; then exit; fi
+
+# continue to execute the command
+echo $0 Proceeding.... at `date`; echo
+
+
