ebb22bf2b4a95f221e48ed6a45359dc38436a91f
Darrin Meek <darrinmeek@aolcom>
2016-03-18 09:00:55 -0400

    March 2016 updates

--- iboaUtils/a.sh
+++ iboaUtils/a.sh
@@ -9,14 +9,7 @@ source $TAGA_CONFIG_DIR/config
 
 echo; echo $0 : $MYIP :  executing at `date`; echo
 
-# issue confirmation prompt
-$iboaUtilsDir/confirm.sh
-
+# issue confirmation prompt and check reponse
+$iboaUtilsDir/confirm.sh $0 "Info to Print"
 # check the response
-let response=$?
-if [ $response -eq 1 ]; then
-  echo; echo Confirmed, $0 continuing....; echo
-else
-  echo; echo Not Confirmed, $0 exiting with no action...; echo
-  exit
-fi
+response=$?; if [ $response -ne 1 ]; then exit; fi
