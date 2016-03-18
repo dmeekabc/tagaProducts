60717e7626672366fea366a1f31e1460d9969f3b
Darrin Meek <darrinmeek@aolcom>
2016-03-18 12:28:29 -0400

    March 2016 updates

--- synchme.sh
+++ synchme.sh
@@ -26,10 +26,15 @@ do
 
      # define the source string
      SCP_SOURCE_STR="."          # use this to synch everything here and below
+     SCP_SOURCE_STR="gitpull.sh" # use this to synch this file only
      SCP_SOURCE_STR="synchme.sh" # use this to synch this file only
 
      # send the files to the destination
      scp -r $SCP_SOURCE_STR darrin@$target:$MYDIR # <$SCRIPTS_DIR/taga/passwd.txt
+
+     # dlm temp, this is work in progress, 
+     # dlm temp, note, currently pulls to root (not what we want)
+     #ssh -l darrin $target $MYDIR/gitpull.sh
 
    fi
 
