539e569ff18661e93f53360ffaec5ca72db67dde
Darrin Meek <darrinmeek@aolcom>
2016-03-19 13:52:22 -0400

    March 2016 updates

--- synchme.sh
+++ synchme.sh
@@ -1,3 +1,7 @@
+#####################################################
+# Copyright 2016 IBOA Corp
+# All Rights Reserved
+#####################################################
 
 TAGA_DIR=~/scripts/taga
 TAGA_CONFIG_DIR=$TAGA_DIR/tagaConfig
@@ -7,13 +11,11 @@ MYDIR=`pwd`
 
 echo $MYDIR
 
-############# 7 lines begin here #############
 # provide the info to print into the confirmation request
 InfoToPrint=" $MYDIR will be synchronized. "
 # issue confirmation prompt and check reponse
 $tagaUtilsDir/confirm.sh $0 "$InfoToPrint"
 response=$?; if [ $response -ne 1 ]; then exit; fi
-############# 7 lines end here #############
 
 echo 
 echo $targetList
@@ -34,20 +36,9 @@ do
 
      # define the source string
      SCP_SOURCE_STR="."          # use this to synch everything here and below
-     SCP_SOURCE_STR="gitpull.sh" # use this to synch this file only
-     SCP_SOURCE_STR="synchme.sh" # use this to synch this file only
 
      # send the files to the destination
      scp -r $SCP_SOURCE_STR darrin@$target:$MYDIR # <$SCRIPTS_DIR/taga/passwd.txt
-
-     # remove old link
-#     ssh -l darrin $target rm ~/scripts/taga 
-     # create new link
-#     ssh -l darrin ln -s $MYDIR ~/scripts/taga
-
-     # dlm temp, this is work in progress, 
-     # dlm temp, note, currently pulls to root (not what we want)
-     #ssh -l darrin $target $MYDIR/gitpull.sh
 
    fi
 done
