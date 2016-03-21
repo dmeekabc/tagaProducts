7e39999ee00b6cac2fafb88d33a7cfd5418fa30a
Darrin Meek <darrinmeek@aolcom>
2016-03-21 19:28:13 -0400

    March 2016 updates

--- tagaScripts/tagaScriptsUtils/checkInterface.sh
+++ tagaScripts/tagaScriptsUtils/checkInterface.sh
@@ -9,7 +9,7 @@ source $TAGA_CONFIG_DIR/config
 
 # this is our poor-mans interprocess communications mechanism
 # it is not elegant but does work
-if cat $NET_RESET_IN_PROG_FLAG_FILE | grep 1 ; then
+if cat $NET_RESET_IN_PROG_FLAG_FILE 2>/dev/null | grep 1 ; then
    echo Notice: Reboot is in progress, 
    echo Notice: Reboot is in progress, 
    echo Notice: This $0 is not permitted to run in this state, exiting with no action!
@@ -38,6 +38,7 @@ else
 
    # only do reset if checks indicate it is necessary
 
+   let flag=0
    let retCodeSum=0
 
    let CHECKVAL=$TARGET_COUNT-1
@@ -58,6 +59,9 @@ else
          echo Unreachable Network Detected, Not much we can do...
          # exit the routine, not much we can do
          exit
+      elif [ $retCode -eq 0 ] ; then
+         # set the flag that we have at least one successful ping
+         let flag=1
       else
          let retCodeSum=$retCodeSum+$retCode
       #   echo retCodeSum: $retCodeSum
@@ -69,6 +73,7 @@ else
       fi
    done
 
+   # if we have failed to ping anybody...
    if [ $retCodeSum -ge $CHECKVAL ] ; then
       # more than one bad return, this implies our interface may be bad
       echo Interface is in suspect state - resetting! 
@@ -79,6 +84,32 @@ else
       echo Interface is in suspect state - setting interface up!
       sudo ifconfig $INTERFACE  up < $TAGA_CONFIG_DIR/passwd.txt
       echo Retcode:$?
-   fi
+   # here is another way to check , this may make the block above unnecessary, 
+   # believe that this one is more demanding than the one above
+   elif [ $flag -eq 0 ] ; then
+      # failure to contact anybody implies our interface may be bad
+     
+      # before we declare failure, let's check against the gateway
+      # get the gateway in case it has changed
+      echo; date; echo Determining GATEWAY....
+      MYGATEWAY=`route | grep default | cut -c16-30`
+      echo GATEWAY: $MYGATEWAY
 
+      echo; echo PINGING GATEWAY: $MYGATEWAY; echo
+      ping -c 1 $MYGATEWAY
+      let retCode=$?
+
+      if [ $retCode -eq 0 ] ; then
+         echo Interface appears healthy, comms with gateway AOK
+      else
+         echo Interface is in suspect state - resetting! 
+         echo Interface is in suspect state - setting interface down!
+         sudo ifconfig $INTERFACE  down < $TAGA_CONFIG_DIR/passwd.txt
+         echo Retcode:$?
+         sleep 5
+         echo Interface is in suspect state - setting interface up!
+         sudo ifconfig $INTERFACE  up < $TAGA_CONFIG_DIR/passwd.txt
+         echo Retcode:$?
+      fi
+   fi
 fi
