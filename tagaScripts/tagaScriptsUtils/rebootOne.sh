a44b486d5ec3dd71aaf0f4e0b3534b0d9713f134
Darrin Meek <darrinmeek@aolcom>
2016-03-21 11:09:30 -0400

    March 2016 updates

--- tagaScripts/tagaScriptsUtils/rebootOne.sh
+++ tagaScripts/tagaScriptsUtils/rebootOne.sh
@@ -7,6 +7,33 @@ TAGA_DIR=~/scripts/taga
 TAGA_CONFIG_DIR=$TAGA_DIR/tagaConfig
 source $TAGA_CONFIG_DIR/config
 
+if [ $# -eq 1 ] ; then
+
+rebootTarget=$1
+
+# issue confirmation prompt
+$tagaUtilsDir/confirm.sh $0 \
+   "WARNING: This command will reboot the following: $rebootTarget"
+# check the response
+response=$?; if [ $response -ne 1 ]; then exit; fi
+
+# reboot
+for target in $rebootTarget
+do
+   echo
+   echo processing $target
+   if [ $target == $MYIP ]; then
+      echo skipping self ...
+      continue
+   fi
+   echo rebooting $target .....
+   ssh -l $MYLOGIN_ID $target sudo reboot <$TAGA_CONFIG_DIR/passwd.txt
+
+done
+echo
+
+else
+
 # issue confirmation prompt
 $tagaUtilsDir/confirm.sh $0 \
    "WARNING: This command will reboot the following: $FIXED_ONE_LIST"
@@ -19,7 +46,7 @@ do
    echo
    echo processing $target
    if [ $target == $MYIP ]; then
-      echo skipping self for now...
+      echo skipping self...
       continue
    fi
 
@@ -29,3 +56,4 @@ do
 done
 echo
 
+fi
