43a926126cbebbb59ce4016e90fcfae94c22efb7
Darrin Meek <darrinmeek@aolcom>
2016-03-21 09:52:43 -0400

    March 2016 updates

--- tagaScripts/tagaScriptsUtils/checkInterface.sh
+++ tagaScripts/tagaScriptsUtils/checkInterface.sh
@@ -8,7 +8,18 @@ TAGA_CONFIG_DIR=$TAGA_DIR/tagaConfig
 source $TAGA_CONFIG_DIR/config
 
 
+let CHECKVAL=$TARGET_COUNT-1
+
+# don't proceed with less than 2 checkval
+if [ $CHECKVAL -lt 2 ]; then
+   let CHECKVAL=2
+fi
+
+#echo $CHECKVAL
+#exit
+
 let retCodeSum=0
+
 
 echo PINGING TARGETS: $targetList
 
@@ -26,13 +37,13 @@ do
       let retCodeSum=$retCodeSum+$retCode
       echo retCodeSum: $retCodeSum
    fi
-   if [ $retCodeSum -gt 1 ] ; then
+   if [ $retCodeSum -ge $CHECKVAL ] ; then
       # we have all the info we need... go ahead and break out
       break
    fi
 done
 
-if [ $retCodeSum -gt 1 ] ; then
+if [ $retCodeSum -ge $CHECKVAL ] ; then
    # more than one bad return, this implies our interface may be bad
    echo Interface is in suspect state - resetting! 
    echo Interface is in suspect state - setting interface down!
