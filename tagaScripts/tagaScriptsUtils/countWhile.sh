fc9d48a969782d83726e51c3ae6239b212441426
Darrin Meek <darrinmeek@aolcom>
2016-03-24 01:08:02 -0400

    March 2016 updates

--- tagaScripts/tagaScriptsUtils/countWhile.sh
+++ tagaScripts/tagaScriptsUtils/countWhile.sh
@@ -7,26 +7,7 @@ TAGA_DIR=~/scripts/taga
 TAGA_CONFIG_DIR=$TAGA_DIR/tagaConfig
 source $TAGA_CONFIG_DIR/config
 
-#echo; echo $0 : $MYIP :  executing at `date`; echo
-
-# provide the info to print into the confirmation request
-#InfoToPrint="$0 Put Your Info To Print Here. $0 "
-
-# issue confirmation prompt and check reponse
-#$tagaUtilsDir/confirm.sh $0 "$InfoToPrint"
-#response=$?; if [ $response -ne 1 ]; then exit; fi
-
-# continue to execute the command
-#echo $0 Proceeding.... at `date`; echo
-
 FILE_TO_CHECK=$1
-
-# print the input description if exists
-#if [ $# -eq 2 ]; then
-#   DESCRIPTION=$2
-#   # print the param input string
-#   printf $DESCRIPTION: 
-#fi
 
 let i=0
 while true
@@ -34,7 +15,8 @@ do
   let i=$i+1
 
   if [ -f $FILE_TO_CHECK ]; then
-    let MODVAL=$i%2
+#    let MODVAL=$i%2
+    let MODVAL=$i%1
     if [ $MODVAL -eq 0 ]; then
         # sleep before the first print
         sleep 1
