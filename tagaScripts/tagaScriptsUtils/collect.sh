89fbb99983cd8d41aec14710e52a8e00ef94adbc
Darrin Meek <darrinmeek@aolcom>
2016-03-27 22:04:07 -0400

    March 2016 updates

--- tagaScripts/tagaScriptsUtils/collect.sh
+++ tagaScripts/tagaScriptsUtils/collect.sh
@@ -17,18 +17,15 @@ do
    # if we are in local mode and target == MYIP , do not use ssh or scp
    if cat $TAGA_LOCAL_MODE_FLAG_FILE 2>/dev/null | grep 1 >/dev/null ; then
       if [ $target == $MYIP ]; then
-        echo processing, collecting files from $target start:`date | cut -c12-20`
         cp /tmp/$TEST_DESCRIPTION* $outputDir
         rm /tmp/$TEST_DESCRIPTION* 2>/dev/null 
       else
-        echo processing, collecting files from $target start:`date | cut -c12-20`
         scp $MYLOGIN_ID@$target:/tmp/$TEST_DESCRIPTION* $outputDir
         ssh -l $MYLOGIN_ID $target rm /tmp/$TEST_DESCRIPTION* 2>/dev/null 
       fi
 
    # normal mode
    else
-      echo processing, collecting files from $target start:`date | cut -c12-20`
       scp $MYLOGIN_ID@$target:/tmp/$TEST_DESCRIPTION* $outputDir
       ssh -l $MYLOGIN_ID $target rm /tmp/$TEST_DESCRIPTION* 2>/dev/null 
    fi
