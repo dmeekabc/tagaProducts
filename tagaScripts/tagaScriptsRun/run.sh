b57f7c076eb39819251752f625d3b3c02aad607c
Darrin Meek <darrinmeek@aolcom>
2016-06-26 14:06:14 -0400

    April 2016 updates

--- tagaScripts/tagaScriptsRun/run.sh
+++ tagaScripts/tagaScriptsRun/run.sh
@@ -7,16 +7,34 @@ TAGA_DIR=~/scripts/taga
 TAGA_CONFIG_DIR=$TAGA_DIR/tagaConfig
 source $TAGA_CONFIG_DIR/config
 
+MYLOCALLOGIN_ID=`$TAGA_UTILS_DIR/loginIdLookup.sh $MYIP | tail -n 1`
+
 # determine the time for traffic to begin flowing
 let trafficStartEpoch=`date +%s`
 let trafficStartEpoch=$trafficStartEpoch+$MGEN_SERVER_INIT_DELAY
 
 for target in $targetList
 do
+
+   TAGA_CONFIG_DIR=$TAGA_DIR/tagaConfig
+   source $TAGA_CONFIG_DIR/config
+
    # determine LOGIN ID for each target
    MYLOGIN_ID=`$TAGA_UTILS_DIR/loginIdLookup.sh $target | tail -n 1`
    # dlm temp , I have no clue why this is needed but it is...
    MYLOGIN_ID=`echo $MYLOGIN_ID` 
+
+   tagaScriptsUtilsDir=`echo $tagaScriptsUtilsDir | sed -e s/$MYLOCALLOGIN_ID/MYLOGIN_ID/g`
+   tagaScriptsUtilsDir=`echo $tagaScriptsUtilsDir | sed -e s/MYLOGIN_ID/$MYLOGIN_ID/g`
+
+   tagaScriptsSimDir=`echo $tagaScriptsSimDir | sed -e s/$MYLOCALLOGIN_ID/MYLOGIN_ID/g`
+   tagaScriptsSimDir=`echo $tagaScriptsSimDir | sed -e s/MYLOGIN_ID/$MYLOGIN_ID/g`
+
+   tagaScriptsTcpdumpDir=`echo $tagaScriptsTcpdumpDir | sed -e s/$MYLOCALLOGIN_ID/MYLOGIN_ID/g`
+   tagaScriptsTcpdumpDir=`echo $tagaScriptsTcpdumpDir | sed -e s/MYLOGIN_ID/$MYLOGIN_ID/g`
+
+   tagaScriptsMgenDir=`echo $tagaScriptsMgenDir | sed -e s/$MYLOCALLOGIN_ID/MYLOGIN_ID/g`
+   tagaScriptsMgenDir=`echo $tagaScriptsMgenDir | sed -e s/MYLOGIN_ID/$MYLOGIN_ID/g`
 
    if echo $BLACKLIST | grep $target >/dev/null ; then
       echo The $target is in the black list, skipping...
