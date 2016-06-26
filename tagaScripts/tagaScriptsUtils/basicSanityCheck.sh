5cd657eab3266f17bbe05578768860ca06c0fd26
Darrin Meek IBOA <darrinmeek@aol.com>
2016-06-26 01:14:46 -0400

    Add files via upload

--- tagaScripts/tagaScriptsUtils/basicSanityCheck.sh
+++ tagaScripts/tagaScriptsUtils/basicSanityCheck.sh
@@ -7,10 +7,8 @@ TAGA_DIR=~/scripts/taga
 TAGA_CONFIG_DIR=$TAGA_DIR/tagaConfig
 source $TAGA_CONFIG_DIR/config
 
-# determine LOGIN ID for each target
+# get MYLOGIN_ID from the config or the loginIdMap file if the file exists
 MYLOGIN_ID=`$TAGA_UTILS_DIR/loginIdLookup.sh $MYIP | tail -n 1`
-
-#echo id:$MYLOGIN_ID
 
 if echo $MYLOGIN_ID | grep GoesHere >/dev/null ; then
    echo
