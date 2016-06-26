9b5ace8247c2e067c7815c97251858cf48557302
Darrin Meek IBOA <darrinmeek@aol.com>
2016-06-26 01:09:28 -0400

    Add files via upload

--- tagaScripts/tagaScriptsUtils/basicSanityCheck.sh
+++ tagaScripts/tagaScriptsUtils/basicSanityCheck.sh
@@ -7,18 +7,23 @@ TAGA_DIR=~/scripts/taga
 TAGA_CONFIG_DIR=$TAGA_DIR/tagaConfig
 source $TAGA_CONFIG_DIR/config
 
+# determine LOGIN ID for each target
+MYLOGIN_ID=`$TAGA_UTILS_DIR/loginIdLookup.sh $MYIP | tail -n 1`
+
+#echo id:$MYLOGIN_ID
+
 if echo $MYLOGIN_ID | grep GoesHere >/dev/null ; then
    echo
    echo WARNING: It appears you have not updated the config file with your configuration information
    echo e.g. [ MYLOGIN_ID: $MYLOGIN_ID ]
    echo
    exit 255
-elif echo $MYPASSWD | grep GoesHere >/dev/null ; then
-   echo
-   echo WARNING: It appears you have not updated the config file with your configuration information
-   echo e.g. [ MYPASSWD: $MYPASSWD ]
-   echo
-   exit 255
+#elif echo $MYPASSWD | grep GoesHere >/dev/null ; then
+#   echo
+#   echo WARNING: It appears you have not updated the config file with your configuration information
+#   echo e.g. [ MYPASSWD: $MYPASSWD ]
+#   echo
+#   exit 255
 else
    echo Basic Check Passed >/dev/null
 fi
