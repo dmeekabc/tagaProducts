e11dc9f53fd69276da365acb9b4e4a1b13ae2492
Darrin Meek <darrinmeek@aol.com>
2016-07-02 09:07:24 -0400

    July 2016 updates

--- tagaScripts/tagaScriptsUtils/loginIdLookup.sh
+++ tagaScripts/tagaScriptsUtils/loginIdLookup.sh
@@ -7,12 +7,24 @@ TAGA_DIR=~/scripts/taga
 TAGA_CONFIG_DIR=$TAGA_DIR/tagaConfig
 source $TAGA_CONFIG_DIR/config
 
+# note this works from general to specific through the config and login map files
 
-SEARCHSTRING=`echo $1 | cut -d. -f1-3`
-SEARCHSTRING=$SEARCHSTRING:
-cat $TAGA_CONFIG_DIR/loginmap.txt | grep $SEARCHSTRING | cut -d: -f 2
+#echo MYLOGIN_ID=$MYLOGIN_ID
 
-SEARCHSTRING=`echo $1`
-SEARCHSTRING=$SEARCHSTRING:
-cat $TAGA_CONFIG_DIR/loginmap.txt | grep $SEARCHSTRING | cut -d: -f 2
+# assume no login map file
+# note this will be superseded (last output wins) if login map file does exist
+echo $MYLOGIN_ID
 
+if [ -f $TAGA_CONFIG_DIR/loginmap.txt ]; then
+
+   # look for network part match
+   SEARCHSTRING=`echo $1 | cut -d. -f1-3`
+   SEARCHSTRING=$SEARCHSTRING:
+   cat $TAGA_CONFIG_DIR/loginmap.txt | grep $SEARCHSTRING | cut -d: -f 2
+
+   # look for more specific exact IP match (last more specific output wins)
+   SEARCHSTRING=`echo $1`
+   SEARCHSTRING=$SEARCHSTRING:
+   cat $TAGA_CONFIG_DIR/loginmap.txt | grep $SEARCHSTRING | cut -d: -f 2
+
+fi
