c821e6a8bb86f66785b073b31f6c42ee5b325428
Darrin Meek <darrinmeek@aolcom>
2016-07-22 00:38:39 -0400

    July 2016 updates

--- tagaScripts/tagaScriptsTest/startOfCycleTests.sh
+++ tagaScripts/tagaScriptsTest/startOfCycleTests.sh
@@ -11,6 +11,8 @@ source $TAGA_CONFIG_DIR/config
 iter=0
 TEST_LABEL="Config1" # default to Config1
 
+echo $0 : Test Label : $TEST_LABEL
+
 # get the input (iteration number and test label) if provided
 if [ $# -ge 1 ] ; then
   iter=$1
@@ -44,12 +46,11 @@ fi
 
 rm /tmp/mark.out 2>/dev/null
 rm /tmp/markSecs1.dat 2>/dev/null
-rm /tmp/$0.Complete.dat 2>/dev/null
+rm /tmp/startOfCycleTests.sh.Complete.dat 2>/dev/null
 
 #START TEST
 
 echo "iter:$iter TEST:$TEST_LABEL In Progress" > /tmp/startOfCycleTests.sh.InProgress.dat
-
 echo StartDTG: `date` > /tmp/startOfTest.dat
 
 #mark1 begin
@@ -58,13 +59,20 @@ date -Ins > /tmp/mark.out
 #mark2 begin
 ~/scripts/taga/tagaScripts/tagaScriptsTimer/timeDeltaCalcSeconds.sh startOfTestTimer
 
-echo $0 : Test Label : $TEST_LABEL
 
+PAD=""
+PAD0="..."
 
 # simulate a test
 echo Simulating a Test for 10 seconds...
-sleep 10
-
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+   PAD="$PAD$PAD0"
+  #echo $PAD$i
+  echo $i$PAD
+  sleep 1
+done
+echo DONE Simulating a Test for 10 seconds...
 
 # WE are DONE
 echo StopDTG:: `date` >> /tmp/startOfTestTimer.dat
@@ -91,5 +99,5 @@ echo;echo;
 # Clear the "In Progress" and Set the "Complete" Flag Files
 
 rm /tmp/startOfCycleTests.sh.InProgress.dat
-echo $0 Complete > /tmp/$0.Complete.dat
+echo Complete > /tmp/startOfCycleTests.sh.Complete.dat
 
