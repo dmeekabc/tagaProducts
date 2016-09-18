c4622a8b10c37081837d1f5d59cde204cc39a4c2
Darrin Meek <darrinmeek@aol.com>
2016-09-18 18:15:17 -0400

    August 2016 updates

---
+++ tagaScripts/tagaScriptsMgen/mgenDistro/tagaMini/tagaScripts/tagaScriptsUtils/loginIdLookup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#######################################################################
+#
+# Copyright (c) IBOA Corp 2016
+#
+# All Rights Reserved
+#                                                                     
+# Redistribution and use in source and binary forms, with or without  
+# modification, are permitted provided that the following conditions 
+# are met:                                                             
+# 1. Redistributions of source code must retain the above copyright    
+#    notice, this list of conditions and the following disclaimer.     
+# 2. Redistributions in binary form must reproduce the above           
+#    copyright notice, this list of conditions and the following       
+#    disclaimer in the documentation and/or other materials provided   
+#    with the distribution.                                            
+#                                                                      
+# THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS   
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED    
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE   
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE     
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT    
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR   
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF           
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT            
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE    
+# USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH     
+# DAMAGE.                                                              
+#
+#######################################################################
+
+TAGA_DIR=/tmp/tagaMini
+TAGA_CONFIG_DIR=$TAGA_DIR/tagaConfig
+source $TAGA_CONFIG_DIR/config
+
+# note this works from general to specific through the config and login map files
+
+#echo MYLOGIN_ID=$MYLOGIN_ID
+
+# assume no login map file
+# note this will be superseded (last output wins) if login map file does exist
+echo $MYLOGIN_ID
+
+if [ -f $TAGA_CONFIG_DIR/loginmap.txt ]; then
+
+   # look for network part match
+   SEARCHSTRING=`echo $1 | cut -d. -f1-3`
+   SEARCHSTRING=$SEARCHSTRING:
+   cat $TAGA_CONFIG_DIR/loginmap.txt | grep $SEARCHSTRING | cut -d: -f 2
+
+   # look for more specific exact IP match (last more specific output wins)
+   SEARCHSTRING=`echo $1`
+   SEARCHSTRING=$SEARCHSTRING:
+   cat $TAGA_CONFIG_DIR/loginmap.txt | grep $SEARCHSTRING | cut -d: -f 2
+
+fi
