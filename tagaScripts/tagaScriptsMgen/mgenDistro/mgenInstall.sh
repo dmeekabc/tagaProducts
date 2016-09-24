c66f9c389ccd83efe8df1c53969c1528098fef05
Darrin Meek <darrinmeek@aolcom>
2016-09-24 11:44:18 -0400

    August 2016 updates

---
+++ tagaScripts/tagaScriptsMgen/mgenDistro/mgenInstall.sh
@@ -0,0 +1,12 @@
+cp ./mgenDistro.tgz /tmp
+cd /tmp
+rm -rf tagaMini
+rm -rf mgenDistro
+tar zxvf mgenDistro.tgz 
+cd mgenDistro
+cp -r tagaMini ..
+
+#source tagaInstallAlias.txt
+#tagaInstall
+./tagaInstall.sh
+
