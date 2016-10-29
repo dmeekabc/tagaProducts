57e996e9cb542343ac71800cc3db8c09a1dd5c6a
Darrin Meek <darrinmeek@aolcom>
2016-10-29 15:52:24 -0400

    October 2016 updates

--- tagaScripts/tagaScriptsTimer/nano.sh
+++ tagaScripts/tagaScriptsTimer/nano.sh
@@ -37,24 +37,18 @@ source $TAGA_CONFIG_DIR/config
 echo; echo $0 : $MYIP :  executing at `date`; echo
 
 # provide the info to print into the confirmation request
-InfoToPrint="$0 Put Your Info To Print Here. $0 "
-
+#InfoToPrint="$0 Put Your Info To Print Here. $0 "
 # issue confirmation prompt and check reponse
-$tagaUtilsDir/confirm.sh $0 "$InfoToPrint"
-response=$?; if [ $response -ne 1 ]; then exit; fi
-
+#$tagaUtilsDir/confirm.sh $0 "$InfoToPrint"
+#response=$?; if [ $response -ne 1 ]; then exit; fi
 # continue to execute the command
-echo $0 Proceeding.... at `date`; echo
-
-mynano=`date +%N` 
-mynanoprev=$mynano
+#echo $0 Proceeding.... at `date`; echo
 
 while true
 do 
-   mynano=`date +%N` 
-   if echo $mynano | grep ^0 ; then
-      echo mynano: $mynano mynanoprev: $mynanoprev
+   if echo `date +%N` | grep ^0 ; then
+      date +%N
+      echo
       exit
    fi
-   mynanoprev=$mynano
 done
