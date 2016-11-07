c547986c27ec16a05969dae1efe4fd7178c607e9
Darrin Meek <darrinmeek@aolcom>
2016-11-06 19:32:18 -0500

    October 2016 updates

---
+++ tagaScripts/tagaScriptsUtils/probewHelp.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#######################################################################
+#
+# Copyright (c) IBOA Corp 2016
+#
+# All Rights Reserved
+#                                                                     
+# Redistribution and use in source and binary forms, with or without  
+# modification, are permitted provided that the following conditions 
+# are met:                                                             
+# 1. Redistributions of source code must retain the above copyright    
+#    notice, this list of conditions and the following disclaimer.     
+# 2. Redistributions in binary form must reproduce the above           
+#    copyright notice, this list of conditions and the following       
+#    disclaimer in the documentation and/or other materials provided   
+#    with the distribution.                                            
+#                                                                      
+# THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS   
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED    
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE   
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE     
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT    
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR   
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF           
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT            
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE    
+# USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH     
+# DAMAGE.                                                              
+#
+#######################################################################
+
+TAGA_DIR=~/scripts/taga
+TAGA_CONFIG_DIR=$TAGA_DIR/tagaConfig
+source $TAGA_CONFIG_DIR/config
+
+# Reinit the files so /tmp doesn't grow too large
+# Reinit the files so /tmp doesn't grow too large
+# Reinit the files so /tmp doesn't grow too large
+echo `date` > /tmp/probew.out
+echo `date` > /tmp/tagaInfo.log
+echo `date` > /tmp/tagaWarn.log
+echo `date` > /tmp/tagaAlarm.log
