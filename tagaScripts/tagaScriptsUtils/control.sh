6d4b475de736a3c6f28c7ad20c383c4cd64c7cc5
Darrin Meek <darrinmeek@aolcom>
2016-12-03 01:06:55 -0500

    October 2016 updates

--- tagaScripts/tagaScriptsUtils/control.sh
+++ tagaScripts/tagaScriptsUtils/control.sh
@@ -30,10 +30,13 @@
 #
 #######################################################################
 
-TAGA_DIR=~/scripts/taga
-TAGA_CONFIG_DIR=$TAGA_DIR/tagaConfig
+TAGA_DIR=/opt/taga
+TAGA_CONFIG_DIR=$TAGA_DIR/config
+source $TAGA_CONFIG_DIR/config
 
 echo; echo $0 : $MYIP :  executing at `date`; echo
+
+function previousScreen {
 
 while true
 do
@@ -41,7 +44,7 @@ do
    source $TAGA_CONFIG_DIR/config
 
    echo
-   echo Getting Modes from Database...
+   echo Getting TAGA Modes and Network Context from Database...
    echo
 
    messageTransferMode="TagaMessageTransferMode"
@@ -51,7 +54,209 @@ do
    clear
 
    # issue the header display
+   echo "                                                                Screen 1 of 2"
+   echo -------------------------------------------------------------------------------
+   echo TAGA Control : `date` 
+   echo -------------------------------------------------------------------------------
+   echo Message-Transfer-Mode: $messageTransferMode
+   echo File-Transfer-Mode...: $fileTransferMode 
+   echo Network Context......: $TAGA_CONTEXT 
+   echo -------------------------------------------------------------------------------
    echo
+   echo Command Options:
+   echo " 1. Network Ping"    
+   echo " 2. Network Probe - Brief"    
+   echo " 3. Network Probe - Verbose"    
+   echo " 4. Network State - Polling-based"    
+   echo " 5. Network State - Polling-based ------ Run/Watch/Repeat"    
+   echo
+   echo "Please Enter Command Choice (1..5) or 'n' for next screen or '0' to Exit:"
+
+   read input
+
+   #echo input:$input
+
+   if [ $input ] ; then
+
+   if [ $input == 'n' ] ; then
+      echo Commmand Choice $input proceeding...
+      clear
+      nextScreen
+      continue
+
+   elif [ $input -eq 1 ] ; then
+      echo Commmand Choice $input proceeding...
+      $HOME/scripts/taga/tagaScripts/tagaScriptsUtils/pingNet.sh singleLoopflag
+
+   elif [ $input -eq 2 ] ; then
+      echo Commmand Choice $input proceeding...
+      $HOME/scripts/taga/tagaScripts/tagaScriptsUtils/probe.sh singleLoopflag
+
+   elif [ $input -eq 3 ] ; then
+      echo Commmand Choice $input proceeding...
+      $HOME/scripts/taga/tagaScripts/tagaScriptsUtils/probew.sh singleLoopflag
+
+   elif [ $input -eq 4 ] ; then
+      echo Commmand Choice $input proceeding...
+      sleep 1; clear; echo; date
+      /opt/taga/scripts/getStateTAGA.sh
+      sleep 2
+
+   elif [ $input -eq 5 ] ; then
+      while true
+         do
+         echo Commmand Choice $input proceeding...
+         sleep 1; clear; echo; date
+         /opt/taga/scripts/getStateTAGA.sh
+         sleep 2
+      done
+
+   elif [ $input -eq 0 ] ; then
+      echo Exiting...
+      exit
+
+   else
+      echo Invalid Commmand Input \($input\) , no action taken!
+      sleep 1
+      continue
+   fi
+   else
+      echo No input provided, no action taken!
+      sleep 1
+      continue
+   fi
+
+   sleep 2
+
+   # Okay, do the output here!
+   clear
+   sleep 1
+
+done
+
+}
+
+
+function nextScreen {
+
+while true
+do
+   # get the config
+   source $TAGA_CONFIG_DIR/config
+
+   echo
+   echo Getting TAGA Modes and Network Context from Database...
+   echo
+
+   messageTransferMode="TagaMessageTransferMode"
+   fileTransferMode="TagaFileTransferMode"
+
+   # Okay, do the output here!
+   clear
+
+   # issue the header display
+   echo "                                                                Screen 2 of 2"
+   echo -------------------------------------------------------------------------------
+   echo TAGA Control : `date` 
+   echo -------------------------------------------------------------------------------
+   echo Message-Transfer-Mode: $messageTransferMode
+   echo File-Transfer-Mode...: $fileTransferMode 
+   echo Network Context......: $TAGA_CONTEXT 
+   echo -------------------------------------------------------------------------------
+   echo
+   echo Command Options:
+   echo " 1. Network Ping"    
+   echo " 2. Network Probe - Brief"    
+   echo " 3. Network Probe - Verbose"    
+   echo " 4. Network State - Polling-based"    
+   echo " 5. Network State - Polling-based ------ Run/Watch/Repeat"    
+   echo
+   echo "Please Enter Command Choice (1..5) or 'p' for previous screen or '0' to Exit:"
+
+   read input
+
+   if [ $input ] ; then
+
+   if [ $input == 'p' ] ; then
+      echo Commmand Choice $input proceeding...
+      clear
+      previousScreen
+      continue
+
+   elif [ $input -eq 1 ] ; then
+      echo Commmand Choice $input proceeding...
+      $HOME/scripts/taga/tagaScripts/tagaScriptsUtils/pingNet.sh singleLoopflag
+
+   elif [ $input -eq 2 ] ; then
+      echo Commmand Choice $input proceeding...
+      $HOME/scripts/taga/tagaScripts/tagaScriptsUtils/probe.sh singleLoopflag
+
+   elif [ $input -eq 3 ] ; then
+      echo Commmand Choice $input proceeding...
+      $HOME/scripts/taga/tagaScripts/tagaScriptsUtils/probew.sh singleLoopflag
+
+   elif [ $input -eq 4 ] ; then
+      echo Commmand Choice $input proceeding...
+      sleep 1; clear; echo; date
+      /opt/taga/scripts/getStateTAGA.sh
+      sleep 2
+
+   elif [ $input -eq 5 ] ; then
+      while true
+         do
+         echo Commmand Choice $input proceeding...
+         sleep 1; clear; echo; date
+         /opt/taga/scripts/getStateTAGA.sh
+         sleep 2
+      done
+
+   elif [ $input -eq 0 ] ; then
+      echo Exiting...
+      exit
+
+   else
+      echo Invalid Commmand Input \($input\) , no action taken!
+      sleep 1
+      continue
+   fi
+   else
+      echo No input provided, no action taken!
+      sleep 1
+      continue
+   fi
+
+   sleep 2
+
+   # Okay, do the output here!
+   clear
+   sleep 1
+
+done
+
+
+}
+
+###############
+# MAIN
+###############
+
+while true
+do
+   # get the config
+   source $TAGA_CONFIG_DIR/config
+
+   echo
+   echo Getting TAGA Modes and Network Context from Database...
+   echo
+
+   messageTransferMode="TagaMessageTransferMode"
+   fileTransferMode="TagaFileTransferMode"
+
+   # Okay, do the output here!
+   clear
+
+   # issue the header display
+   echo "                                                                Screen 1 of 2"
    echo -------------------------------------------------------------------------------
    echo TAGA Control : `date`
    echo -------------------------------------------------------------------------------
@@ -64,13 +269,24 @@ do
    echo " 1. Network Ping"    
    echo " 2. Network Probe - Brief"    
    echo " 3. Network Probe - Verbose"    
+   echo " 4. Network State - Polling-based"    
+   echo " 5. Network State - Polling-based ------ Run/Watch/Repeat"    
    echo
-   echo "Please Enter Command Choice (1|2|3) or '0' to Exit:"
+   echo "Please Enter Command Choice (1..5) or 'n' for next screen or '0' to Exit:"
 
    read input
 
    if [ $input ] ; then
-   if [ $input -eq 1 ] ; then
+
+   #echo input:$input
+
+   if [ $input == 'n' ] ; then
+      echo Commmand Choice $input proceeding...
+      clear
+      nextScreen
+      continue
+
+   elif [ $input -eq 1 ] ; then
       echo Commmand Choice $input proceeding...
       $HOME/scripts/taga/tagaScripts/tagaScriptsUtils/pingNet.sh singleLoopflag
 
@@ -81,6 +297,21 @@ do
    elif [ $input -eq 3 ] ; then
       echo Commmand Choice $input proceeding...
       $HOME/scripts/taga/tagaScripts/tagaScriptsUtils/probew.sh singleLoopflag
+
+   elif [ $input -eq 4 ] ; then
+      echo Commmand Choice $input proceeding...
+      sleep 1; clear; echo; date
+      /opt/taga/scripts/getStateTAGA.sh
+      sleep 2
+
+   elif [ $input -eq 5 ] ; then
+      while true
+         do
+         echo Commmand Choice $input proceeding...
+         sleep 1; clear; echo; date
+         /opt/taga/scripts/getStateTAGA.sh
+         sleep 2
+      done
 
    elif [ $input -eq 0 ] ; then
       echo Exiting...
