e28ca2bc0438812dadbc7502d8a49bab1cea462b
Darrin Meek <darrinmeek@aolcom>
2017-04-23 01:21:32 -0400

    April 2017 updates

--- tagaScripts/tagaScriptsTcpdump/tcpdump.sh
+++ tagaScripts/tagaScriptsTcpdump/tcpdump.sh
@@ -64,12 +64,12 @@ fi
 if $TAGA_CONFIG_DIR/hostList.sh | grep `hostname` >/dev/null ; then
   echo Running /usr/sbin/tcpdump on `hostname` > $STATUS_FILE 
   if [ $TAGA_DISPLAY_SETTING -ge $TAGA_DISPLAY_ENUM_VAL_4_VERBOSE ]; then
-    /usr/sbin/tcpdump -n -s 200 -i $MYINTERFACE $myproto port $SOURCEPORT -l                       \
+    /usr/sbin/sudo /usr/sbin/tcpdump -n -s 200 -i $MYINTERFACE $myproto port $SOURCEPORT -l                       \
      <$TAGA_CONFIG_DIR/passwd.txt | tee                                                  \
      /tmp/$TEST_DESCRIPTION\_`hostname`_$MYINTERFACE\_$MY_PARAM_IP\_`date +%j%H%M%S` 
   else
     #echo /usr/sbin/tcpdump -n -s 200 -i $MYINTERFACE $myproto port $SOURCEPORT -l       
-    /usr/sbin/tcpdump -n -s 200 -i $MYINTERFACE $myproto port $SOURCEPORT -l                       \
+    /usr/sbin/sudo /usr/sbin/tcpdump -n -s 200 -i $MYINTERFACE $myproto port $SOURCEPORT -l                       \
      <$TAGA_CONFIG_DIR/passwd.txt >                                                      \
          /tmp/$TEST_DESCRIPTION\_`hostname`_$MYINTERFACE\_$MY_PARAM_IP\_`date +%j%H%M%S` \
               2>/dev/null
