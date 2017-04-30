27cabc43c4eecc69df9450ec8ee6c9edb8632887
Darrin Meek <darrinmeek@aolcom>
2017-04-30 11:58:43 -0400

    April 2017 updates

--- tagaScripts/tagaScriptsStats/countSends.sh
+++ tagaScripts/tagaScriptsStats/countSends.sh
@@ -200,20 +200,27 @@ echo; echo >> $TAGA_RUN_DIR/counts.txt
 # build up the buffer
 buffer1="TAGA:Iter:$iter: Tot Files:`ls $outputDir | wc -l` Rec'd Count:$printCount / $expectedCount exp msgs "
 
-if [ $MAX_STAT_DISPLAY -eq 0 ] ;then
-   echo MAX DISLAY is OFF but the Recd count is always displayed for UCAST_UDP!! >/dev/null
-elif [ $TESTTYPE == "MCAST" ]; then
-   echo MAX DISPLAY is ON but MCAST>/dev/null
-   buffer1=""
-elif [ $TESTTYPE == "UCAST_TCP" ]; then
-   echo MAX DISPLAY is ON but UCAST_TCP>/dev/null
-   #buffer1=""
-elif [ $TAGA_TRAFFIC_GENERATOR == "BASH_SOCKET" ] ; then
-   echo MAX DISPLAY is ON but BASH_SOCKET>/dev/null
-   buffer1=""
-elif [ $TAGA_TRAFFIC_GENERATOR == "IPERF" ] ; then
-   echo MAX DISPLAY is ON but IPERF>/dev/null
-   buffer1=""
+if [ $EXPERT_DISPLAY -eq 1 ] ;then
+   # Expert display is on
+   echo Nothing to Filter since Expert Display has no filters >/dev/null
+else
+   # Expert display is off
+   # Check Filters for reduced display
+   if [ $MAX_STAT_DISPLAY -eq 0 ] ;then
+      echo MAX DISLAY is OFF but the Recd count is always displayed for UCAST_UDP!! >/dev/null
+   elif [ $TESTTYPE == "MCAST" ]; then
+      echo MAX DISPLAY is ON but MCAST>/dev/null
+      buffer1=""
+   elif [ $TESTTYPE == "UCAST_TCP" ]; then
+      echo MAX DISPLAY is ON but UCAST_TCP>/dev/null
+      #buffer1=""
+   elif [ $TAGA_TRAFFIC_GENERATOR == "BASH_SOCKET" ] ; then
+      echo MAX DISPLAY is ON but BASH_SOCKET>/dev/null
+      buffer1=""
+   elif [ $TAGA_TRAFFIC_GENERATOR == "IPERF" ] ; then
+      echo MAX DISPLAY is ON but IPERF>/dev/null
+      buffer1=""
+   fi
 fi
 
 # pad the buffer
