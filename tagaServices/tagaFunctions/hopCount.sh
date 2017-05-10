ef5ac6cecc876ac8edd76d03a95c5487ccae42e4
Darrin Meek <darrinmeek@aolcom>
2017-05-10 19:55:09 -0400

    May 2017 updates

---
+++ tagaServices/tagaFunctions/hopCount.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#######################################################################
+#
+# Copyright (c) IBOA Corp 2017
+#
+# All Rights Reserved
+#                                                                     
+# Redistribution and use in source and binary forms, with or without  
+# modification, are permitted provided that the following conditions 
+# are met:                                                             
+# 1. Redistributions of source code must retain the above copyright    
+#    notice, this list of conditions and the following disclaimer.     
+# 2. Redistributions in binary form must reproduce the above           
+#    copyright notice, this list of conditions and the following       
+#    disclaimer in the documentation and/or other materials provided   
+#    with the distribution.                                            
+#                                                                      
+# THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS   
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED    
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE   
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE     
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR  
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT    
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR   
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF           
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT            
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE    
+# USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH     
+# DAMAGE.                                                              
+#
+#######################################################################
+
+TAGA_DIR=/cf/var/home/jtm
+TAGA_CONFIG_DIR=$TAGA_DIR/tagaConfig
+source $TAGA_CONFIG_DIR/config
+
+echo; echo $0 : $MYIP :  executing at `date`; echo
+
+# provide the info to print into the confirmation request
+InfoToPrint="$0 Put Your Info To Print Here. $0 "
+
+# issue confirmation prompt and check reponse
+$tagaUtilsDir/confirm.sh $0 "$InfoToPrint"
+response=$?; if [ $response -ne 1 ]; then exit; fi
+
+# continue to execute the command
+echo $0 Proceeding.... at `date`; echo
+
+for target in $targetList
+do
+   echo `date` : TAGA: traceroute $target
+   hopCount=`traceroute $target | tail -n 1 | cut -c2-3`
+   echo Target:$target HopCount:$hopCount
+   echo Target:$target HopCount:$hopCount
+   traceroute $target
+   retCode=$?
+   echo retCode: $retCode
+   echo
+done
+
